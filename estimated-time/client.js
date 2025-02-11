4c85392545d43c822737c98bd654024b4a582617
David Schubert <github@davidschubert.com>
2025-02-10 19:07:43 -1000

    mhh

--- estimated-time/client.js
+++ estimated-time/client.js
@@ -3,11 +3,11 @@ TrelloPowerUp.initialize({
         return [
             {
                 icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
-                text: "⏱️ Estimated Time",
+                text: "Estimated Time",
                 /*condition: "always",
                 callback: function (t) {
                     return t.popup({
-                        title: "⏱️ Estimated Time",
+                        title: "Estimated Time",
                         url: "popup.html",
                         height: 150,
                     });
