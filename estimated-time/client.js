4a7d7aba53d258dd95d23dfea833c52deb9a1baf
David Schubert <github@davidschubert.com>
2025-02-10 19:45:07 -1000

    board buttons

--- estimated-time/client.js
+++ estimated-time/client.js
@@ -16,6 +16,23 @@ TrelloPowerUp.initialize({
             },
         ];
     },
+    "board-buttons": function (t, options) {
+        return [
+            {
+                icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
+                text: "Estimated Time",
+                //condition: "always",
+                callback: function (t) {
+                    return t.board().then((board) => alert(`Hello world!`));
+                    /*return t.popup({
+                        title: "Estimated Time",
+                        url: "popup.html",
+                        height: 150,
+                    });*/
+                },
+            },
+        ];
+    },
     /*"card-detail-badges": function (t, options) {
         console.log("card-detail-badges wird aufgerufen.");
         return t
